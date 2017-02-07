700b4ae9db57d27a9981413e6c38e25b7f101c90
rewenset <a.zakurenyi@gmail.com>
2017-02-07 15:57:56 +0200

    Added asynchronus tasks to orders app

--- myshop/orders/views.py
+++ myshop/orders/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 from .models import OrderItem
 from .forms import OrderCreateForm
+from .tasks import order_created
 from cart.cart import Cart
 
 
@@ -19,6 +20,7 @@ def order_create(request):
                     quantity=item['quantity']
                 )
             cart.clear()
+            order_created.delay(order.id)
             return render(request,
                           'orders/order/created.html',
                           {'order': order})
